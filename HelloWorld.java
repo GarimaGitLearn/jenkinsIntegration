4c68f42ec2f10527c36cfd732d43dc5ad6641924
GarimaGitLearn <86945890+GarimaGitLearn@users.noreply.github.com>
2022-02-01 18:57:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
  public static void main(String args[]){
-   System.out.println("--------------*****Running Fnal java file through github and jenkins integration****------");
+   System.out.println("--------------*****Running Fnal jasafva file through github and jenkins integration****------");
 }
 }
